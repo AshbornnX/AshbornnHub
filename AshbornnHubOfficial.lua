ffe77859b3ea4a97fe7629525b6372cb181ec89c
Ashbornn <ashbornn21@gmail.com>
2025-07-04 23:12:28 +0800

    Update AshbornnHubOfficial.lua

--- AshbornnHubOfficial.lua
+++ AshbornnHubOfficial.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/4377673e69bc1ef7"))()
+local success, err = pcall(function()
+    loadstring(game:HttpGet("https://loader.pandadevelopment.net/virtual/file/4377673e69bc1ef7"))()
+end)
+
+if not success then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ashborrn/AshborrnHub/refs/heads/main/key-system-backup"))()
+end
